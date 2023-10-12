ba3ca6e4016a7b04a642cf28ae8cebb878a5af0f
KK-Hyeok <128603307+KK-Hyeok@users.noreply.github.com>
2023-10-12 17:48:27 +0900

    2024-10-12
    end

---
+++ ch4/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:ch4/PuzzleUI.dart';
+import 'package:flutter/material.dart';
+import 'ContainerTest.dart';
+import 'StackTest.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //home: const ContainerTest(),
+      home: const PuzzleUI(),
+    );
+  }
+}
+
+
+
