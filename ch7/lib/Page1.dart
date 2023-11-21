f119d3190fe506c17e9582828b286d89116f3594
KK-Hyeok <128603307+KK-Hyeok@users.noreply.github.com>
2023-11-21 17:50:02 +0900

    2023-11-21
    end

--- ch7/lib/Page1.dart
+++ ch7/lib/Page1.dart
@@ -1,13 +1,16 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
+    return ListView(
+      children: <Widget>[
         _buildTop(),
         _buildMiddle(),
         _buildBottom(),
@@ -117,10 +120,45 @@ class Page1 extends StatelessWidget {
   }
 
   Widget _buildMiddle() {
-    return Text('Middle');
+    final CatItems = [
+      'assets/cat1.jpg',
+      'assets/cat2.jpg',
+      'assets/cat3.jpg'
+    ];
+    return CarouselSlider(
+        options: CarouselOptions(height: 400.0,),
+        items:CatItems.map((path){
+          return Builder(
+            builder: (BuildContext context){
+            return Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 5.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(
+                  path,
+                  fit: BoxFit.cover,
+              ),
+            )
+          );
+        },
+          );
+      }).toList(),
+    );
   }
 
   Widget _buildBottom() {
-    return Text('Bottom');
+  final items = List.generate(
+      10,(i){
+        return ListTile(
+          leading: Icon(Icons.notifications_none),
+          title: Text('공지사항입니다.'),
+        );
+  });
+  return ListView(
+    physics: NeverScrollableScrollPhysics(),
+    shrinkWrap: true,
+    children: items,
+  );
   }
 }
